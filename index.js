d76db25c0ad33362c7aef2ac1bf47a1f7ce0f9af
SalamunHasan69e <salamun2600@gmail.com>
2022-11-27 00:40:36 +0600

    get categories

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
@@ -10,15 +11,33 @@ app.use(express.json())
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.3kn3bwn.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3kn3bwn.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
 
+async function run() {
+
+  try {
+    const categoriesCollection = client.db('smartShop').collection('categories');
+
+    app.get('/categories', async (req, res) => {
+      const query = {};
+      const cursor = categoriesCollection.find(query);
+      const categories = await cursor.toArray();
+      res.send(categories);
+    });
+
+    app.get('/products', async (req, res) => {
+
+    })
+  }
+
+  finally {
+
+  }
+
+}
+run().catch(error => console.log(error));
 
 app.get('/', async (req, res) => {
   res.send('smart shop server is running')
