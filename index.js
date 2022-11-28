2c53cc7dcc577defa1a02fd256d4982cfb1772de
SalamunHasan69e <salamun2600@gmail.com>
2022-11-28 12:06:17 +0600

    get booking API

--- index.js
+++ index.js
@@ -36,6 +36,13 @@ async function run() {
       res.send(result)
     });
 
+    app.get('/bookings', async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
+    })
+
     app.post('/bookings', async (req, res) => {
       const booking = req.body
       console.log(booking);
